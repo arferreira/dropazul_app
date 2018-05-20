8e1b4bff036980e2f5fcb7d6e17a5750b64aa3c3
Antonio Ricardo Ferreira de Souza <antonioricardoarfs@gmail.com>
2018-05-20 14:59:15 -0400

    Insert layout on landing app

--- atrix_landing/urls.py
+++ atrix_landing/urls.py
@@ -2,7 +2,9 @@ from django.urls import path, include
 
 from atrix_landing.views import LandingPageView
 
+app_name="atrix_landing"
+
 urlpatterns = [
     # root route
-    path('', LandingPageView.as_view()),
+    path('', LandingPageView.as_view(), name='home'),
 ]
--- atrixmob/urls.py
+++ atrixmob/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from atrix_landing import urls
+# url para a landing page
+from atrix_landing import urls as landing_urls
 
 urlpatterns = [
-    # root route
-    path('', include(urls)),
+    # root route - Landing page
+    path('', include(landing_urls, namespace='landing-page')),
     # admin django route
     path('admin/', admin.site.urls),
 ]
