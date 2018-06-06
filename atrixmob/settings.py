d604d7a1a8ee77999646dfb28c4081b414b3e689
Antonio Ricardo Ferreira de Souza <antonioricardoarfs@gmail.com>
2018-06-05 22:29:14 -0400

    Refactor settings of project

--- atrixmob/settings.py
+++ atrixmob/settings.py
@@ -113,12 +113,25 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_LOCATION = 'static'
 
+
+
 # static files
-STATIC_ROOT = 'staticfiles/'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
 
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATICFILES_DIRS = (os.path.join('static'),)
+# STATIC_ROOT = 'staticfiles/'
+#
+# STATIC_URL = '/static/'
+#
+# STATICFILES_DIRS = (os.path.join('static'),)
 
 
 
