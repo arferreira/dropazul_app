36cf946775d44cd46ec527af67db61a030485edc
Antonio Ricardo Ferreira de Souza <antonioricardoarfs@gmail.com>
2018-06-09 12:58:38 -0400

    Change to aws s3 bucket staticfiles

--- atrixmob/settings.py
+++ atrixmob/settings.py
@@ -104,34 +104,34 @@ TENANT_MODEL = 'atrix_tenant.Client'
 # Static amazon s3
 
 
-AWS_ACCESS_KEY_ID = 'AKIAJMGGRT5DDWMHA4VQ'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'atrixmobcore'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-AWS_LOCATION = 'static'
-
-
-
-# static files
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-AWS_LOCATION = 'static'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-# STATIC_ROOT = 'staticfiles/'
+# AWS_ACCESS_KEY_ID = 'AKIAJMGGRT5DDWMHA4VQ'
+# AWS_SECRET_ACCESS_KEY = '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'
+# AWS_STORAGE_BUCKET_NAME = 'atrixmobcore'
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+# AWS_S3_OBJECT_PARAMETERS = {
+#     'CacheControl': 'max-age=86400',
+# }
+# AWS_LOCATION = 'static'
 #
-# STATIC_URL = '/static/'
 #
-# STATICFILES_DIRS = (os.path.join('static'),)
+#
+# # static files
+# AWS_S3_OBJECT_PARAMETERS = {
+#     'CacheControl': 'max-age=86400',
+# }
+# AWS_LOCATION = 'static'
+#
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+STATIC_ROOT = 'staticfiles/'
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join('static'),)
 
 
 
