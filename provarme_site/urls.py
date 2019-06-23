93485849f938b51a00b9de26822aa8b6e4720a43
Antonio Ricardo <antonioricardoarfs@gmail.com>
2019-06-23 17:18:43 -0400

    Finalizando rastreio de encomendas

--- provarme_site/urls.py
+++ provarme_site/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 
-from provarme_site.views import IndexPageView
+from provarme_site.views import IndexPageView, tracking_order
 
 app_name="provarme_site"
 
 urlpatterns = [
     # root route
     path('', IndexPageView.as_view(), name='index'),
+    path('rastreio/', tracking_order, name='tracking_order'),
+
+
 ]
--- provarme_site/views.py
+++ provarme_site/views.py
@@ -1,5 +1,10 @@
 # responses django
+import json
+import requests
 from django.shortcuts import render, HttpResponse
+from correios import Correios
+from django.views.decorators.csrf import csrf_protect
+
 
 # CBVs Django
 from django.views.generic.base import TemplateView
@@ -7,3 +12,19 @@ from django.views.generic.base import TemplateView
 
 class IndexPageView(TemplateView):
     template_name = 'provarme_site/index_site.html'
+
+
+
+def tracking_order(request):
+    if request.method == "GET":
+        template = "provarme_site/tracking_order.html"
+        eventos = {}
+    elif request.method == "POST":
+        template = "provarme_site/tracking_result.html"
+        code = request.POST.get('code', None)
+        url = "https://api.postmon.com.br/v1/rastreio/ect/{}".format(code)
+        data = requests.get(url).json()
+
+        eventos = data['historico']
+    return render(request, template, context={'eventos': eventos})
+
